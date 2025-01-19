2173533b3826237117876832724174c291f21ac7
xavcochran <le22092@bristol.ac.uk>
2025-01-19 12:24:29 +0000

    pushing built helix mod for ease

--- helix-website/src/app/api.ts
+++ helix-website/src/app/api.ts
@@ -12,37 +12,7 @@ interface Return {
   newGraphData: Record<string, Record<string, any>>;
 }
 
-export async function executeQuery(id: string, queryName: string, queryContent: string, schemaContent: string): Promise<Return> {
-  try {
-    console.log("API_CONFIG.BASE_URL", API_CONFIG.BASE_URL);
-    const response = await fetch(`${API_CONFIG.BASE_URL}/query`, {
-      method: 'POST',
-      headers: API_CONFIG.DEFAULT_HEADERS,
-      body: JSON.stringify({ queryName, queryContent, schemaContent, id }),
-    });
-    let query_response = await response.json();
 
-
-    console.log('query_response', query_response);
-    return { result: query_response.result.result, newGraphData: query_response.result.graph_data };
-  } catch (error) {
-    console.error('Error executing query:', error);
-    throw error;
-  }
-}
-
-export async function init(id: string): Promise<void> {
-  try {
-    await fetch(`${API_CONFIG.BASE_URL}/init`, {
-      method: 'POST',
-      headers: API_CONFIG.DEFAULT_HEADERS,
-      body: JSON.stringify({ userID: id }),
-    });
-  } catch (error) {
-    console.error('Error initializing handler:', error);
-    throw error;
-  }
-}
 
 class API {
   id: string;
@@ -52,10 +22,12 @@ class API {
   }
 
   init(id: string): void {
+    let body = JSON.stringify({ userID: id });
+    console.log("body", body);
     fetch(`${API_CONFIG.BASE_URL}/init`, {
       method: 'POST',
       headers: API_CONFIG.DEFAULT_HEADERS,
-      body: JSON.stringify({ userID: id }),
+      body:body,
     }).then((response) => {
       if (!response.ok) {
         throw new Error('Failed to initialize handler');
