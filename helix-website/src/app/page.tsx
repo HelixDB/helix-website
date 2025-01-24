9441b9ee2103ca522d9abcd5727c8cded290c8a5
Gand4lf <Georgejamescurtis@hotmail.co.uk>
2025-01-24 22:44:52 +0000

    Testing this works online

--- helix-website/src/app/page.tsx
+++ helix-website/src/app/page.tsx
@@ -170,9 +170,8 @@ E::Follows {
         setGraphData({ nodes, edges });
       }
     } catch (err: any) {
-      const errorOutput = `[${new Date().toLocaleTimeString()}] Error running query: ${
-        err.message
-      }\n\n`;
+      const errorOutput = `[${new Date().toLocaleTimeString()}] Error running query: ${err.message
+        }\n\n`;
       setOutput((prev) => prev + "\n" + errorOutput);
     } finally {
       setIsLoading(false);
@@ -195,151 +194,150 @@ E::Follows {
         >
           <div className="flex items-center w-full justify-between mb-6">
             <Logo />
-            <div className="flex gap-2">
-              <button
-                onClick={() => setActiveView("terminal")}
-                className={`px-4 py-2 rounded-full flex items-center gap-2 transition-all ${
-                  activeView === "terminal"
-                    ? "bg-primary text-primary-foreground shadow-sm"
-                    : "text-muted-foreground hover:text-foreground"
-                }`}
-              >
-                <svg
-                  className="-ms-0.5 me-1.5 opacity-60"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <polyline points="4 17 10 11 4 5"></polyline>
-                  <line x1="12" y1="19" x2="20" y2="19"></line>
-                </svg>
-                Terminal
-              </button>
-              <button
-                onClick={() => {
-                  setActiveView("graph");
-                  // loadSampleData();
-                }}
-                className={`px-4 py-2 rounded-full flex items-center gap-2 transition-all ${
-                  activeView === "graph"
-                    ? "bg-primary text-primary-foreground shadow-sm"
-                    : "text-muted-foreground hover:text-foreground"
-                }`}
-              >
-                <svg
-                  className="-ms-0.5 me-1.5 opacity-60"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <circle cx="12" cy="12" r="10"></circle>
-                  <line x1="2" y1="12" x2="22" y2="12"></line>
-                  <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
-                </svg>
-                Graph
-              </button>
-            </div>
-            <div className="flex items-center gap-2">
+
+            <div className="flex items-center gap-4">
               <ThemeToggle />
-              <Button variant="outline" size="icon" asChild>
-                <a
-                  href="https://github.com/HelixDB/helix-db"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Github className="h-4 w-4 text-neutral-500" />
+              <Button variant="outline" size="icon" className="text-neutral-500">
+                <a href="https://github.com/HelixDB/helix-db" target="_blank" rel="noopener noreferrer">
+                  <Github className="h-4 w-4" />
+                </a>
+              </Button>
+              <Button variant="outline" className="text-neutral-500" >
+                <a href="https://helix.mintlify.app/introduction" target="_blank" rel="noopener noreferrer" >
+                  Docs
                 </a>
               </Button>
             </div>
           </div>
 
-          {activeView === "terminal" ? (
-            <div className="mx-auto">
-              <div className="w-full">
-                <div className="grid md:grid-cols-2 gap-6">
-                  <div className="">
-                    <div className="border border-b-0 bg-muted/50">
-                      <button className="px-3 py-2 text-sm font-medium border-r relative text-foreground bg-background mb-[2px]">
-                        schema.hx
-                      </button>
-                    </div>
-                    <div
-                      className=""
-                      style={{
-                        background:
-                          currentTheme === "dark"
-                            ? "rgba(255,255,255,0.1)"
-                            : "rgba(255,255,255,0.5)",
-                      }}
-                    >
-                      <CodeEditor
-                        isEditable={false}
-                        value={schema}
-                        onChange={(e) => setSchema(e.target.value)}
-                        placeholder="Enter your schema here..."
-                        style={{ resize: "none", background: "transparent" }}
-                      />
+          <div className="mx-auto">
+            <div className="w-full">
+              <div className="grid md:grid-cols-2 gap-6">
+                <div className="">
+                  <div className="border border-b-0 bg-muted/50">
+                    <button className="px-3 py-2 text-sm font-medium border-r relative text-foreground bg-background mb-[2px]">
+                      schema.hx
+                    </button>
+                  </div>
+                  <div
+                    className=""
+                    style={{
+                      background:
+                        currentTheme === "dark"
+                          ? "rgba(255,255,255,0.1)"
+                          : "rgba(255,255,255,0.5)",
+                    }}
+                  >
+                    <CodeEditor
+                      isEditable={false}
+                      value={schema}
+                      onChange={(e) => setSchema(e.target.value)}
+                      placeholder="Enter your schema here..."
+                      style={{ resize: "none", background: "transparent" }}
+                    />
+                  </div>
+                </div>
+
+                <div className="">
+                  <div className="border border-b-0 bg-muted/50">
+                    <div className="flex">
+                      {Object.keys(queries).map((filename) => (
+                        <button
+                          key={filename}
+                          onClick={() => handleTabClick(filename)}
+                          className={`px-3 py-2 text-sm font-medium border-r relative ${activeQuery === filename
+                            ? "text-foreground bg-background mb-[2px]"
+                            : "text-muted-foreground hover:text-foreground hover:bg-muted/70"
+                            }`}
+                        >
+                          {filename}
+                        </button>
+                      ))}
                     </div>
                   </div>
-
-                  <div className="">
-                    <div className="border border-b-0 bg-muted/50">
-                      <div className="flex">
-                        {Object.keys(queries).map((filename) => (
-                          <button
-                            key={filename}
-                            onClick={() => handleTabClick(filename)}
-                            className={`px-3 py-2 text-sm font-medium border-r relative ${
-                              activeQuery === filename
-                                ? "text-foreground bg-background mb-[2px]"
-                                : "text-muted-foreground hover:text-foreground hover:bg-muted/70"
-                            }`}
-                          >
-                            {filename}
-                          </button>
-                        ))}
-                      </div>
-                    </div>
-                    <div
-                      className=""
-                      style={{
-                        background:
-                          currentTheme === "dark"
-                            ? "rgba(255,255,255,0.1)"
-                            : "rgba(255,255,255,0.5)",
-                      }}
-                    >
-                      <CodeEditor
-                        value={query}
-                        onChange={(e) => handleQueryChange(e.target.value)}
-                        placeholder="Enter your query here..."
-                        style={{ resize: "none", background: "transparent" }}
-                      />
-                    </div>
+                  <div
+                    className=""
+                    style={{
+                      background:
+                        currentTheme === "dark"
+                          ? "rgba(255,255,255,0.1)"
+                          : "rgba(255,255,255,0.5)",
+                    }}
+                  >
+                    <CodeEditor
+                      value={query}
+                      onChange={(e) => handleQueryChange(e.target.value)}
+                      placeholder="Enter your query here..."
+                      style={{ resize: "none", background: "transparent" }}
+                    />
                   </div>
                 </div>
               </div>
-              <div className="flex justify-end items-center gap-2 my-4">
-                <Button
-                  onClick={handleRun}
-                  disabled={isLoading}
-                  className="gap-2 rounded-none"
+            </div>
+            <div className="flex justify-between items-center gap-2 my-4">
+              <div className="flex gap-2">
+                <button
+                  onClick={() => setActiveView("terminal")}
+                  className={`px-4 py-2 rounded-full flex items-center gap-2 transition-all ${activeView === "terminal"
+                    ? "bg-primary text-primary-foreground shadow-sm"
+                    : "text-muted-foreground hover:text-foreground"
+                    }`}
                 >
-                  <PlayIcon className="w-4 h-4" />
-                  Run Query
-                </Button>
+                  <svg
+                    className="-ms-0.5 me-1.5 opacity-60"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <polyline points="4 17 10 11 4 5"></polyline>
+                    <line x1="12" y1="19" x2="20" y2="19"></line>
+                  </svg>
+                  Terminal
+                </button>
+                <button
+                  onClick={() => {
+                    setActiveView("graph");
+                    // loadSampleData();
+                  }}
+                  className={`px-4 py-2 rounded-full flex items-center gap-2 transition-all ${activeView === "graph"
+                    ? "bg-primary text-primary-foreground shadow-sm"
+                    : "text-muted-foreground hover:text-foreground"
+                    }`}
+                >
+                  <svg
+                    className="-ms-0.5 me-1.5 opacity-60"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <circle cx="12" cy="12" r="10"></circle>
+                    <line x1="2" y1="12" x2="22" y2="12"></line>
+                    <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
+                  </svg>
+                  Graph
+                </button>
               </div>
+              <Button
+                onClick={handleRun}
+                disabled={isLoading}
+                className="gap-2"
+              >
+                <PlayIcon className="w-4 h-4" />
+                Run Query
+              </Button>
+            </div>
 
+            {activeView === "terminal" ? (
               <div className="space-y-4">
                 <div
                   className="border rounded-none "
@@ -353,31 +351,33 @@ E::Follows {
                   <div className="border-b px-3 py-2 text-sm font-medium bg-muted/50">
                     output.log
                   </div>
-                  <pre className="p-4 font-mono text-sm overflow-auto max-h-[300px]">
+                  <pre className="p-4 font-mono text-sm overflow-auto max-h-[500px] overflow-auto">
                     {output}
                   </pre>
                 </div>
               </div>
-            </div>
-          ) : (
-            <div className="relative border rounded-none overflow-hidden ">
-              <div className="border-b px-3 py-2 text-sm font-medium bg-muted/50">
-                Graph Visualization
-              </div>
 
-              <div className="h-[500px] w-full">
-                <GraphVisualizer
-                  data={{
-                    nodes: graphData?.nodes || [],
-                    links: graphData?.edges || [],
-                  }}
-                  // width={}
-                  currentTheme={currentTheme}
-                />
+            ) : (
+              <div className="relative border rounded-none overflow-hidden ">
+                <div className="border-b px-3 py-2 text-sm font-medium bg-muted/50">
+                  Graph Visualization
+                </div>
+
+                <div className="h-[500px] w-full">
+                  <GraphVisualizer
+                    data={{
+                      nodes: graphData?.nodes || [],
+                      links: graphData?.edges || [],
+                    }}
+                    // width={}
+                    currentTheme={currentTheme}
+                  />
+                </div>
               </div>
-            </div>
-          )}
+            )}
+          </div>
         </div>
+
       </WavyBackground>
     </main>
   );
