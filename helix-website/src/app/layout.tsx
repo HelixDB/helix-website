5dbfb2abadd64d1349a65ebb755eec6c676b0146
Gand4lf <Georgejamescurtis@hotmail.co.uk>
2025-01-26 13:04:21 +0000

    fixing module import

--- helix-website/src/app/layout.tsx
+++ helix-website/src/app/layout.tsx
@@ -3,10 +3,6 @@ import { Geist } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/header";
 import { Providers } from "@/components/providers";
-import { Amplify } from "aws-amplify";
-import config from "../aws-exports";
-
-Amplify.configure(config);
 
 const geist = Geist({
   subsets: ["latin"],
