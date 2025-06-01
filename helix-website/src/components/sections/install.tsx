c512f58dc962e819443867796633343471c75f91
Gand4lf <Georgejamescurtis@hotmail.co.uk>
2025-06-01 10:13:08 -0700

    fixing docs button

--- helix-website/src/components/sections/install.tsx
+++ helix-website/src/components/sections/install.tsx
@@ -213,7 +213,7 @@ export function Install() {
                         size="lg"
                         asChild
                     >
-                        <a href="/docs">
+                        <a href="https://docs.helix-db.com">
                             <span>Docs</span>
                             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
